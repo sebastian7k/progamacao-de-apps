56f4c70048b631320f61b8a203eb620f321c49de
Sebastian7k <delanemaia@gmail.com>
2026-03-05 16:47:41 -0300

    ,

---
+++ funcao.js
@@ -0,0 +1,29 @@
+// Funções comuns em JavaScript
+
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return a / b;
+}
+
+// Chamando as funções
+const soma = somar(2, 3);
+const subtracao = subtrair(2, 3);
+const multiplicar = multiplicacao(2, 3);
+const dividir = divisao(2, 3);
+
+// Mostrando os resultados
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicar);
+console.log("Divisão:", dividir);
